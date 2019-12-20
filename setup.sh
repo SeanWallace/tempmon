eaabbaba03803e7393ed18b004f4762b4656a814
Sean Wallace <swallace@cray.com>
2019-12-20 10:07:06 -0600

    Updated setup script.

--- setup.sh
+++ setup.sh
@@ -2,14 +2,18 @@
 
 set -ex
 
-sudo apt update
-sudo apt install -y python3 python3-pip git
+apt update
+apt install -y python3 python3-pip git
+
+mkdir -p /etc/tempmon
 
 pip3 install -r requirements.txt
 
 ln -f -s "$(pwd)/init/tempmon.service" /etc/systemd/system/tempmon.service
 systemctl daemon-reload
 systemctl enable tempmon.service
-systemctl restart tempmon.service
 
 ln -f -s "$(pwd)/cron/tempmon.sh" /etc/cron.hourly/tempmon
+
+echo 'Install complete. Make sure to put a config file at /etc/tempmon/config.json and then run `systemctl start
+tempmon.service`.'
